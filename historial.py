c3cc9e452f8d4392722ae30d8d37a24a0e0b9512
Lucas Jaime <113988656+jaimelucas01@users.noreply.github.com>
2026-05-22 18:22:24 -0300

    Delete historial.py

--- historial.py
+++
@@ -1,9 +0,0 @@
-def mostrar_historial(historial):
-    print("Mostrando historial...")
-
-def guardar_historial(historial):
-    print("Historial guardado.")
-
-def cargar_historial(historial):
-    print("Historial cargado.")
-    return historial
